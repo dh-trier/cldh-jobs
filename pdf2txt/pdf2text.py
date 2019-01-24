215c8051788a76292b7907fbff743555026ddc3a
christofs <c.schoech@gmail.com>
2019-01-24 14:17:18 +0100

    mit identifier

--- pdf2txt/pdf2text.py
+++ pdf2txt/pdf2text.py
@@ -7,7 +7,7 @@ See: https://textract.readthedocs.io/en/stable/index.html
 
 """
 Installation (on Ubuntu, for Python3): 
-(1) Install required packages: apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libasound-dev libpulse-dev
+(1) Install required packages: sudo apt-get install python-dev libxml2-dev libxslt1-dev antiword unrtf poppler-utils pstotext tesseract-ocr flac ffmpeg lame libmad0 libsox-fmt-mp3 sox libjpeg-dev swig libasound-dev libpulse-dev
 (2) pip3 install pocketsphinx
 (3) pip3 install textract
 """
@@ -18,12 +18,14 @@ Installation (on Ubuntu, for Python3):
 import os
 import glob
 import textract
+import datetime
 
 
 # === Parameters ===
 
 pdfs = os.path.join("pdf", "*.pdf")
 txts = "txt"
+plattform = "misc"
 
 
 # === Functions ===
@@ -39,17 +41,24 @@ def read_pdf(pdf):
     return text, filename
 
 
-def save_text(text, filename, txts): 
+def generate_identifier(plattform, pdf, filename): 
+    identifier = plattform + '_' + datetime.datetime.now().strftime('%d-%m-%y-%H-%M-%S-%f')
+    print(identifier)
+    return identifier
+
+
+
+def save_text(text, identifier, txts): 
     """
     Saves the string to a file with the same filename as the input file.
     """
-    with open(os.path.join(txts, filename + ".txt"), "w", encoding="utf8") as outfile: 
+    with open(os.path.join(txts, identifier + ".txt"), "w", encoding="utf8") as outfile: 
         outfile.write(text)
 
 
 # === Coordinating function ===
 
-def main(pdfs, txts):
+def main(plattform, pdfs, txts):
     """
     Coordinates the PDF to TXT conversion for all files contained in the
     folder indicated in "pdfs". 
@@ -60,6 +69,7 @@ def main(pdfs, txts):
         os.makedirs(txts)
     for pdf in glob.glob(pdfs): 
         text, filename = read_pdf(pdf)
-        save_text(text, filename, txts)
+        identifier = generate_identifier(plattform, pdf, filename)
+        save_text(text, identifier, txts)
     
-main(pdfs, txts)
+main(plattform, pdfs, txts)

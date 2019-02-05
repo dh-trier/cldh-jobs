87ba10344a0940a88cf29ee6321ebd696d4fe088
scherzod <ortikov@yahoo.de>
2019-02-06 00:20:36 +0100

    neue Methode fuer Radius Groesse

--- geomapping/Radius.py
+++ geomapping/Radius.py
@@ -2,7 +2,7 @@ def methode_schoech(total_jobs):
     radius = float(((total_jobs)/8)+3)
     return radius
 
-#Verteilt die Werte zwischen 5 und 50.
+#Verteilt die Werte zwischen 4 und 50.
 #Wert 444 muss nach dubletten Erkennung angepasst werden. 444 ist aktuell der groesste Wert.
 def methode_rosch(total_jobs):
     radius = float((total_jobs-1)*45/((444-1)-1)+4)
